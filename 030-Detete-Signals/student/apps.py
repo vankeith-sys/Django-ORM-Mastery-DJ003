4dac4717a21ea15a60142adb741e7f09c223085b
Van Keith <vkalmazan@gmail.com>
2023-02-27 22:01:31 +0800

    finished apps.py

--- 030-Detete-Signals/student/apps.py
+++ 030-Detete-Signals/student/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 class StudentsConfig(AppConfig):
     name = 'student'
 
-
+    def ready(self):
+        from . import signals
+        # connecting apps.py to the signals file
