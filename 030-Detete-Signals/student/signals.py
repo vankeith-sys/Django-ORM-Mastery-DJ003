6d0ce915d90b080809c875c764ea674fc02de014
Van Keith <vkalmazan@gmail.com>
2023-02-27 21:40:26 +0800

    finished pre delete and post delete signals

--- 030-Detete-Signals/student/signals.py
+++ 030-Detete-Signals/student/signals.py
@@ -1,3 +1,11 @@
 from django.db.models.signal import pre_delete, post_delete
 from .models import Student
+from django.dispatch import receiver
 
+@receiver(pre_delete, sender=Student)
+def pre_delete(sender, **kwargs):
+    print('You are about to delete sumn')
+
+@receiver(post_delete, sender=Student)
+def post_delete(ender, **kwargs):
+    print('You just deleted a student or sumn')
