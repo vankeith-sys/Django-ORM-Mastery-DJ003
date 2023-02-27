bea658987830f877a0bb22a1ce689ee3c0efa21a
Van Keith <vkalmazan@gmail.com>
2023-02-27 22:19:44 +0800

    added pre and post save with instance as arguments

--- 030-Detete-Signals/student/signals.py
+++ 030-Detete-Signals/student/signals.py
@@ -1,4 +1,9 @@
-from django.db.models.signals import pre_delete, post_delete
+from django.db.models.signals import (
+    pre_delete,
+    post_delete,
+    pre_save,
+    post_save,
+)
 from .models import Student
 from django.dispatch import receiver
 
@@ -9,3 +14,10 @@ def pre_delete(sender, **kwargs):
 @receiver(post_delete, sender=Student)
 def post_delete(sender, **kwargs):
     print('You just deleted a student or sumn')
+
+@receiver(pre_save, sender=Student)
+def pre_save(sender, instance: Student, **kwargs):
+    print('******** START PRE SAVE ********')
+    print(instance)
+    print(type(instance))
+    print('******** END PRE SAVE ********')
